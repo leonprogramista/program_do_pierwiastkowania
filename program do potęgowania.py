77a3de6cf30bc25d2e433f3be28d023f1e44785d
leonprogramista <146365530+leonprogramista@users.noreply.github.com>
2024-03-15 22:52:22 +0100

    Add files via upload

---
+++ program do potęgowania.py
@@ -0,0 +1,17 @@
+liczba = int (input ('podaj liczbe ktora chcesz potengowac: '))
+ilosc_poteng = int('input(podaj potenge liczby (ta u gory): '))
+wynik = liczba
+
+if liczba = 0:
+    print ('0')
+elif liczba = 1:
+    print( liczba )
+else:
+    if ilosc_poteng = 0:
+            print('1')
+    elif ilosc_poteng = 1:
+            print( liczba )
+    else:
+        for i in range():
+           wynik =*liczba 
+        print(wynik)
